7892099f9057feeb03f64a39cb5c5eed60d859c0
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-07-17 19:07:37 +0200

    Added secret verification

--- listener/listener.py
+++ listener/listener.py
@@ -1,16 +1,53 @@
-from flask import Flask, request
+import hashlib
+import hmac
 import json
+
+import werkzeug.exceptions
+from flask import Flask, request
 
 app = Flask(__name__)
 
 COMMUNICATION_PATH = "/comm/fifo"
+GITHUB_SECRET_PATH = "/comm/github_secrets.json"
 
 
-@app.route("/<webhook>", methods=["POST"])
+def verify_signature(payload_body: bytes, hookname: str, signature_header: str | None):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+
+    with open(GITHUB_SECRET_PATH, "r", encoding="utf-8") as secretfile:
+        secrets = json.load(secretfile)
+        if hookname not in secrets:
+            return
+        secret_token = secrets[hookname]
+
+    if not signature_header:
+        raise werkzeug.exceptions.Forbidden("x-hub-signature-256 header is missing!")
+    hash_object = hmac.new(
+        secret_token.encode("utf-8"), msg=payload_body, digestmod=hashlib.sha256
+    )
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise werkzeug.exceptions.Forbidden("Request signatures didn't match!")
+
+
+@app.route("/github/<webhook>", methods=["POST"])
 def hello_world(webhook: str):
     content = request.json
     if content is None:
-        return "Content body not json", 415
+        raise werkzeug.exceptions.UnsupportedMediaType("Content body not json")
+    verify_signature(
+        request.get_data(), webhook, request.headers["x-hub-signature-256"]
+    )
     with open(COMMUNICATION_PATH, "w", encoding="utf-8") as commfile:
-        commfile.write(json.dumps({"hook": webhook, "content": content}))
+        commfile.write(
+            json.dumps({"hook": webhook, "source": "GITHUB", "content": content})
+        )
     return "Done", 200
