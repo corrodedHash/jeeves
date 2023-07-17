667b35c083745746465a002831938bf4aa316f5e
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-07-17 20:16:19 +0200

    Added codenames webhook

--- actor/main.py
+++ actor/main.py
@@ -1,17 +1,33 @@
 import argparse
 import json
 from typing import Any
+import subprocess
 
 
 def handle_hook(path: str, payload: Any):
-    print(path, "\n", payload)
+    if path == "codenames":
+        if payload.get("ref", None) != "refs/heads/main":
+            return
+        subprocess.run(
+            ["git", "pull"],
+            cwd="/home/maint/docker-setup/codenames/codenames/codenames",
+            check=True,
+        )
+        subprocess.run(
+            "docker compose up --build -d",
+            cwd="/home/maint/docker-setup/codenames/codenames/codenames",
+            check=True,
+        )
 
 
 def loop(path: str):
     while True:
         with open(path, "r", encoding="utf-8") as commfile:
             request = json.loads(commfile.read())
-            handle_hook(request["hook"], request["content"])
+            hookname = request["hook"]
+            hookcontent = request["content"]
+            print(f"Handling hook '{hookname}'")
+            handle_hook(hookname, hookcontent)
 
 
 def main() -> None:
