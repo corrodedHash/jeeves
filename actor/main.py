98bc1c8774d88147ec08ac440e3f5b477332ed3f
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-08-20 23:56:02 +0200

    Added apps handler

--- actor/main.py
+++ actor/main.py
@@ -3,24 +3,46 @@ import json
 from typing import Any
 import subprocess
 
+from pathlib import Path
 
-def handle_hook(path: str, payload: Any):
+
+def handle_codenames(payload: Any) -> None:
+    if payload.get("ref", None) != "refs/heads/main":
+        return
+    subprocess.run(
+        ["runuser", "-u", "maint", "--", "git", "pull"],
+        cwd="/home/maint/docker-setup/codenames/codenames/codenames",
+        check=True,
+    )
+    subprocess.run(
+        ["docker", "compose", "up", "--build", "-d"],
+        cwd="/home/maint/docker-setup/codenames/",
+        check=True,
+    )
+
+
+def handle_hook(path: str, payload: Any) -> None:
     if path == "codenames":
-        if payload.get("ref", None) != "refs/heads/main":
-            return
-        subprocess.run(
-            ["runuser", "-u", "maint", "--", "git", "pull"],
-            cwd="/home/maint/docker-setup/codenames/codenames/codenames",
-            check=True,
-        )
+        handle_codenames(payload)
+        return
+    if path.startswith("apps-"):
+        for x in Path("/home/maint/docker-setup/apps/apps-docker/apps-auto").iterdir():
+            if not x.is_dir():
+                return
+            print(x, x.absolute())
+            subprocess.run(
+                ["runuser", "-u", "maint", "--", "git", "pull"],
+                cwd=str(x),
+                check=True,
+            )
         subprocess.run(
             ["docker", "compose", "up", "--build", "-d"],
-            cwd="/home/maint/docker-setup/codenames/codenames/codenames",
+            cwd="/home/maint/docker-setup/apps",
             check=True,
         )
 
 
-def loop(path: str):
+def loop(path: str) -> None:
     while True:
         with open(path, "r", encoding="utf-8") as commfile:
             request = json.loads(commfile.read())
