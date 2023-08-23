fe86c7615a7263127dd71c01687d17e942e54332
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-08-23 16:51:53 +0200

    Made logging a little less verbose

--- actor/main.py
+++ actor/main.py
@@ -113,7 +113,6 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    logging.basicConfig()
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
